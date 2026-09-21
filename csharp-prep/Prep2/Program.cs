4fa12cb77fa70d25471a33cfbdc91972887cc65e
will <willh999880@gmail.com>
2026-09-21 09:59:37 -0600

    made changes to csprep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        string input = Console.ReadLine();
+        int percentage = int.Parse(input);
+        string letter = "A" ;
+
+
+        if (percentage >= 90)
+        {
+           letter = "A";
+        } 
+        else if (percentage >= 80) 
+        {
+           letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = ("D");
+        }
+        else
+        {
+            letter = "F";
+
+        }
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You failed the class better luck next time.");
+        }
+
     }
 }
