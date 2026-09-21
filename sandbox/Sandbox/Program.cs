9805c9294d5fcc8aa325f954eccb59585e94b4bc
will <willh999880@gmail.com>
2026-09-21 09:31:24 -0600

    added else if and else statments to my if

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -8,8 +8,17 @@ class Program
 
         if (x == 0)
         {
-         Console.WriteLine("X equals 10");
+         Console.WriteLine("X is 10");
          Console.WriteLine("Y is fun");
+        }
+        else if (x == 20)
+        {
+            Console.WriteLine("We are in the else if. ");
+        }
+        else
+        {
+            Console.WriteLine("Z is not much fun.");
+
         }
 
     }
