b49f63512c249bdd322b766617f32e544a9d1a56
will <willh999880@gmail.com>
2026-09-23 09:22:08 -0600

    while loop

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,27 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        int x = 10;
-
-        int z = 30;
-
-        int y = 20;
-
-
-        if (x == 10 ||  y == 20 && z== 30)
+       bool done = false;
+       while( ! done)
         {
-         Console.WriteLine("X is 10");
-         Console.WriteLine("Y is fun");
+            Console.Write("Are we done yet? y/n ");
+            done = Console.ReadLine().ToLower() == "y";
         }
-        else if (x == 20)
-        {
-            Console.WriteLine("We are in the else if. ");
-        }
-        else
-        {
-            Console.WriteLine("Z is not much fun.");
-
-        }
-
     }
 }
